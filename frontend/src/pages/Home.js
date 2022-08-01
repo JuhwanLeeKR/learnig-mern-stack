bcf2e728faa2ee9d0d91698c5a4b59cc012ef73f
Juhwan <dev.juhwanlee@gmail.com>
2022-08-02 01:50:55 +0900

    Feat: create WorkoutDetails component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from 'react';
+
+// components
+import WorkoutDetails from '../components/WorkoutDetails';
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts');
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className='home'>
-      <h2>Home</h2>
+      <div className='workouts'>
+        {workouts &&
+          workouts.map((workout) => (
+            <div key={workout._id}>
+              <WorkoutDetails key={workout.id} workout={workout} />
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
