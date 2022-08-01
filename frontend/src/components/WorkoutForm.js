4148786911cd29586414bd513628c439152f8c42
Juhwan <dev.juhwanlee@gmail.com>
2022-08-02 02:40:15 +0900

    Feat: status management by context API

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
 const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
+
   const [title, setTitle] = useState('');
   const [load, setLoad] = useState('');
   const [reps, setReps] = useState('');
@@ -29,6 +32,7 @@ const WorkoutForm = () => {
       setReps('');
       setError(null);
       console.log('new workout added', json);
+      dispatch({ type: 'CREATE_WORKOUT', payload: json });
     }
   };
 
