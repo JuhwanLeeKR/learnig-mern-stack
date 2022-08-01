50044b77c16e086ff2419df502908a62882c3509
Juhwan <dev.juhwanlee@gmail.com>
2022-08-01 23:13:25 +0900

    Feat: fix get API, add error messages

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,29 +1,20 @@
 const express = require('express');
-const Workout = require('../models/workoutModel');
+const {
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+} = require('../controllers/workoutController');
 
 const router = express.Router();
 
 // GET all workouts
-router.get('/', (req, res) => {
-  res.json({ msg: 'GET all workouts' });
-});
+router.get('/', getWorkouts);
 
 // GET a single workout
-router.get('/:id', (req, res) => {
-  res.json({ msg: 'GET a single workout' });
-});
+router.get('/:id', getWorkout);
 
 // POST a new workout
-router.post('/', async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post('/', createWorkout);
 
 // DELETE a new workout
 router.delete('/:id', (req, res) => {
